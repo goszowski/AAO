0786da65c02a887d9b53f7f36dba164cb92ee2f8
Jaroslaw Goszowski <goszowski@gmail.com>
2018-06-03 13:14:07 +0300

    slider

--- views/_layouts/resources.blade.php
+++ views/_layouts/resources.blade.php
@@ -15,6 +15,7 @@
 <body>
 	@yield('app')
 
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.min.js"></script>
 	@yield('page-scripts')
 </body>
--- views/root/show.blade.php
+++ views/root/show.blade.php
@@ -4,14 +4,54 @@
 
 @section('content')
 
-	<ul class="home-slider">
+	<ul class="home-slider owl-carousel">
+
+		@for($i=1; $i<=4; $i++)
 		<li class="home-slider__item">
 			<span class="home-slider__item__title">
-				<span>Lorem ipsum dolor</span>
-				Fotografia spałniająca oczekiwania
+				<span>Lorem ipsum dolor {{ $i }}</span>
+				Fotografia spałniająca oczekiwania {{ $i }}
 			</span>
 			<img src="/assets/dist/images/demo/slider-1.jpg" alt="The title of the slide">
 		</li>
+		@endfor
+
 	</ul>
 
+@endsection
+
+@section('page-scripts')
+<script>
+	$(function() {
+		var homeSliderAutoplayTimeout = 6000;
+		var homeSlider = $('.home-slider');
+		var homeSliderRunProgress = function() {
+			var dot = $('.home-slider .owl-dot.active .progress');
+			dot.stop();
+			dot.css('width', '0%');
+			dot.animate({'width': '100%'}, homeSliderAutoplayTimeout);
+		}
+
+		homeSlider.on('initialized.owl.carousel', function(event) {
+			setTimeout(function() {
+				$('.home-slider .owl-dot').append('<div class="line"></div><div class="progress"></div>');
+				homeSliderRunProgress();
+			}, 50);
+		});
+
+		var homeSlider = homeSlider.owlCarousel({
+			items: 1,
+			loop: true,
+			dots: true,
+			autoplay: true,
+			autoplayTimeout: homeSliderAutoplayTimeout,
+		});
+
+		homeSlider.on('change.owl.carousel', function(event) {
+			setTimeout(homeSliderRunProgress, 50);
+		});
+
+		
+	});
+</script>
 @endsection
