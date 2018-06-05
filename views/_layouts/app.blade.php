e0fed8302f47dfdca69bebc2a9906f6272f7c16e
Jaroslaw Goszowski <goszowski@gmail.com>
2018-06-05 14:13:09 +0300

    початок меню

--- views/_layouts/app.blade.php
+++ views/_layouts/app.blade.php
@@ -12,6 +12,42 @@
 			<span class="menu-toggler__default-state"></span>
 			<span class="menu-toggler__hover-state"></span>
 		</button>
+
+		<div id="navigation">
+			<nav class="main-nav">
+				<ul>
+					<li>
+						<a class="active" href="/portfolio">Portfolio</a>
+					</li>
+					<li>
+						<a href="/portfolio">Oferta</a>
+					</li>
+					<li>
+						<a href="/portfolio">Studio</a>
+					</li>
+					<li>
+						<a href="/portfolio">Aktualności</a>
+					</li>
+					<li>
+						<a href="/portfolio">Kontakt</a>
+					</li>
+				</ul>
+			</nav>
+
+			<nav class="footer-nav">
+				<ul>
+					<li>
+						<a href="#">Polityka cookies</a>
+					</li>
+					<li>
+						<a href="#">Digital signage</a>
+					</li>
+					<li>
+						<a href="#">Praca</a>
+					</li>
+				</ul>
+			</nav>
+		</div>
 	</header>
 
 	{{-- Page content --}}
