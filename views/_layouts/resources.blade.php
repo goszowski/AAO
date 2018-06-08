57d88407d774593db5fa043a5f3fcbc6e090cd47
Bodya3927 <bodyahnedko@gmail.com>
2018-06-08 17:45:09 +0300

    правки скрипта меню ы скролинга для black-objects

--- views/_layouts/resources.blade.php
+++ views/_layouts/resources.blade.php
@@ -21,8 +21,17 @@
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.min.js"></script>
 	
 	<script>
-		$('.menu-toggler').on('click', function(){
 
+	var color = 0; //white-objects
+
+	$(function(){
+		if($('header').hasClass('black-objects')) {
+			color = 1;
+		}
+	});
+
+	$('.menu-toggler').on('click', function(){
+		if($('header').hasClass('white-objects')) {
 			if($('header').hasClass('scrolled')) {
 				$('#navigation').addClass('open');
 				$('body').addClass('navigation-is-open');
@@ -36,9 +45,24 @@
 					$('header').removeClass('white-objects').addClass('black-objects');
 				}, 700);
 			}
-		});
+		}
+		else {
+			if($('header').hasClass('scrolled')) {
+				$('#navigation').addClass('open');
+				$('body').addClass('navigation-is-open');
+				$('html').addClass('ovf-hidd');
+			}
+			else {
+				$('#navigation').addClass('open');
+				$('body').addClass('navigation-is-open');
+				$('html').addClass('ovf-hidd');
+			}
+		}
 
-		$('.menu-close').on('click', function(){
+	});
+
+	$('.menu-close').on('click', function(){
+		if (color == 0) {
 			if($('header').hasClass('scrolled')) {
 				$('#navigation').removeClass('open');
 				$('body').removeClass('navigation-is-open');
@@ -50,9 +74,118 @@
 				$('html').removeClass('ovf-hidd');
 				$('header').removeClass('black-objects').addClass('white-objects');
 			}
-		});
-	</script>
+		}
+		else {
+			if($('header').hasClass('scrolled')) {
+				$('#navigation').removeClass('open');
+				$('body').removeClass('navigation-is-open');
+				$('html').removeClass('ovf-hidd');
+			}
+			else {
+				$('#navigation').removeClass('open');
+				$('body').removeClass('navigation-is-open');
+				$('html').removeClass('ovf-hidd');
+			}
+		}
 
-	@yield('page-scripts')
+	});
+
+	$(document).ready(function(){
+		$('#up').click(function() {
+			$('html, body').animate({scrollTop: 0},700);
+			return false;
+		});
+
+	var header = $("header"); // Меню
+	var	header_h = header.height() + 22;
+	var scrollPrev = 0 // Предыдущее значение скролла
+	
+	$(window).scroll(function() {
+		var scrolled = $(window).scrollTop(); // Высота скролла в px
+		var firstScrollUp = false; // Параметр начала сколла вверх
+		var firstScrollDown = false; // Параметр начала сколла вниз
+		
+		// Если скроллим
+		if ( scrolled > 0 ) {
+			// Если текущее значение скролла > предыдущего, т.е. скроллим вниз
+			if ( scrolled > scrollPrev ) {
+				firstScrollUp = false; // Обнуляем параметр начала скролла вверх
+				// Если меню видно
+				if ( scrolled < header.height() + header.offset().top ) {
+					// Если только начали скроллить вниз
+					if ( firstScrollDown === false ) {
+						var topPosition = header.offset().top; // Фиксируем текущую позицию меню
+						header.css({
+							"top": topPosition + "px"
+						});
+						firstScrollDown = true;
+					}
+					// Позиционируем меню абсолютно
+					header.css({
+						"position": "absolute"
+					});
+				// Если меню НЕ видно
+			} else {
+					// Позиционируем меню фиксированно вне экрана
+					header.css({
+						"position": "fixed",
+						"top": "-" + header_h + "px"
+					});
+				}
+				
+			// Если текущее значение скролла < предыдущего, т.е. скроллим вверх
+		} else {
+				firstScrollDown = false; // Обнуляем параметр начала скролла вниз
+				// Если меню не видно
+				if ( scrolled > header.offset().top ) {
+					// Если только начали скроллить вверх
+					if ( firstScrollUp === false ) {
+						var topPosition = header.offset().top; // Фиксируем текущую позицию меню
+						header.css({
+							"top": topPosition + "px"
+						});
+						console.log('c = ' + color);
+						if(color == 0) {
+							$(header).removeClass('white-objects').addClass('black-objects').addClass('scrolled');
+						}
+						else {
+							$(header).addClass('scrolled');
+						}
+						firstScrollUp = true;
+					}
+					// Позиционируем меню абсолютно
+					header.css({
+						"position": "absolute"
+					});
+				} else {
+					// Убираем все стили
+					header.removeAttr("style");
+					header.css({
+						"background-color": "#fff"
+					});
+				}
+			}
+			// Присваеваем текущее значение скролла предыдущему
+			scrollPrev = scrolled;
+		}
+		if(scrolled == 0) {
+			header.css({
+				"background-color": "transparent"
+			});
+			if(color == 0) {
+				$(header).removeClass('black-objects').removeClass('scrolled').addClass('white-objects');
+			} else {
+				$(header).removeClass('scrolled');
+			}
+		}
+	});	
+
+
+});
+
+
+</script>
+
+@yield('page-scripts')
 </body>
 </html>
--- views/portfolio/show.blade.php
+++ views/portfolio/show.blade.php
@@ -1,6 +1,6 @@
 @extends('_layouts.app')
 
-@section('header-class', 'white-objects')
+@section('header-class', 'black-objects')
 
 @section('content')
 
@@ -97,120 +97,7 @@
 
 @section('page-scripts')
 <script>
-	$(function() {
-		var homeSliderAutoplayTimeout = 6000;
-		var homeSlider = $('.home-slider');
-		var homeSliderRunProgress = function() {
-			var dot = $('.home-slider .owl-dot.active .progress');
-			dot.stop();
-			dot.css('width', '0%');
-			dot.animate({'width': '100%'}, homeSliderAutoplayTimeout);
-		}
 
-		homeSlider.on('initialized.owl.carousel', function(event) {
-			setTimeout(function() {
-				$('.home-slider .owl-dot').append('<div class="line"></div><div class="progress"></div>');
-				homeSliderRunProgress();
-			}, 50);
-		});
-
-		var homeSlider = homeSlider.owlCarousel({
-			items: 1,
-			loop: true,
-			dots: true,
-			autoplay: true,
-			autoplayTimeout: homeSliderAutoplayTimeout,
-		});
-
-		homeSlider.on('change.owl.carousel', function(event) {
-			setTimeout(homeSliderRunProgress, 50);
-		});
-
-		
-	});
-
-	$(document).ready(function(){
-		$('#up').click(function() {
-			$('html, body').animate({scrollTop: 0},700);
-			return false;
-		})
-
-	var header = $("header"); // Меню
-	var	header_h = header.height() + 22;
-	var scrollPrev = 0 // Предыдущее значение скролла
-	
-	$(window).scroll(function() {
-		var scrolled = $(window).scrollTop(); // Высота скролла в px
-		var firstScrollUp = false; // Параметр начала сколла вверх
-		var firstScrollDown = false; // Параметр начала сколла вниз
-		
-		// Если скроллим
-		if ( scrolled > 0 ) {
-			// Если текущее значение скролла > предыдущего, т.е. скроллим вниз
-			if ( scrolled > scrollPrev ) {
-				firstScrollUp = false; // Обнуляем параметр начала скролла вверх
-				// Если меню видно
-				if ( scrolled < header.height() + header.offset().top ) {
-					// Если только начали скроллить вниз
-					if ( firstScrollDown === false ) {
-						var topPosition = header.offset().top; // Фиксируем текущую позицию меню
-						header.css({
-							"top": topPosition + "px"
-						});
-						firstScrollDown = true;
-					}
-					// Позиционируем меню абсолютно
-					header.css({
-						"position": "absolute"
-					});
-				// Если меню НЕ видно
-				} else {
-					// Позиционируем меню фиксированно вне экрана
-					header.css({
-						"position": "fixed",
-						"top": "-" + header_h + "px"
-					});
-				}
-				
-			// Если текущее значение скролла < предыдущего, т.е. скроллим вверх
-			} else {
-				firstScrollDown = false; // Обнуляем параметр начала скролла вниз
-				// Если меню не видно
-				if ( scrolled > header.offset().top ) {
-					// Если только начали скроллить вверх
-					if ( firstScrollUp === false ) {
-						var topPosition = header.offset().top; // Фиксируем текущую позицию меню
-						header.css({
-							"top": topPosition + "px"
-						});
-						$(header).removeClass('white-objects').addClass('black-objects').addClass('scrolled');
-						firstScrollUp = true;
-					}
-					// Позиционируем меню абсолютно
-					header.css({
-						"position": "absolute"
-					});
-				} else {
-					// Убираем все стили
-					header.removeAttr("style");
-					header.css({
-							"background-color": "#fff"
-						});
-				}
-			}
-			// Присваеваем текущее значение скролла предыдущему
-			scrollPrev = scrolled;
-		}
-		if(scrolled == 0) {
-			header.css({
-				"background-color": "transparent"
-			});
-			$(header).removeClass('black-objects').removeClass('scrolled').addClass('white-objects');
-		}
-	});	
-
-
-	});
 
 </script>
 @endsection
