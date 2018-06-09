2c2346de9c5daf4019f437326a6dbf05122b201a
Bodya3927 <bodyahnedko@gmail.com>
2018-06-09 14:02:32 +0300

    сторінка studio

--- router.php
+++ router.php
@@ -2,5 +2,7 @@
 
 $router->get('/', function() use($blade) {
 	//return $blade->make('root/show');
-	return $blade->make('portfolio-index/show');
+	//return $blade->make('portfolio/show');
+	//return $blade->make('portfolio-index/show');
+	return $blade->make('studio/show');
 });
---
+++ views/studio/show.blade.php
@@ -0,0 +1,109 @@
+@extends('_layouts.app')
+
+@section('header-class', 'black-objects')
+
+@section('content')
+
+<!--Хедер повинен мати клас black-object-->
+
+<div class="title-block clearfix text-xs-md-center">
+	<div class="inner-block pl-172">
+		<p class="title-block_small-title">NAPIS TEKST</p>
+		<h2 class="title-block_big-title">Studio</h2>
+	</div>
+	<div class="inner-block pr-172">
+		<p class="title-block_descr">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perspiciatis quod facere laboriosam! Porro, fugiat! Aspernatur maxime recusandae neque deserunt quaerat eum animi est, quia enim! Consequuntur quasi ad nam voluptate.</p>
+	</div>
+</div>
+
+
+<div class="studio-inner-block clearfix pl-172 pr-172">
+	<img src="assets/dist/images/demo/im4.jpg" alt="d">
+		<div class="studio-inner-block_descr">
+			<h3 class="inner-block_link__big-title fz36">Nazwa Kolejna</h3>
+			<p class="inner-block_link__small-title">PODPIS</p>
+			<p class="title-block_desc">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco. laboris nisi ut aliquip ex ea commodo consequat. </p>
+		</div>
+</div>
+
+
+<div class="studio-text">
+	<h3>Opis 1</h3>
+	<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco. laboris nisi ut aliquip ex ea commodo consequat. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco. laboris nisi ut aliquip ex ea commodo consequat.</p>
+	<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco. laboris nisi ut aliquip ex ea commodo consequat. </p>
+	<ul>
+		<li>Lorem ipsum dolor sit amet, consectetur adipiscing elit</li>
+		<li>Ssed do eiusmod tempor incididunt ut labore et dolore magna aliqua</li>
+		<li>Ut enim ad minim veniam, quis nostrud exercitation ullamco.</li>
+		<li>Nisi ut aliquip ex ea commodo consequat</li>
+	</ul>
+	<img src="assets/dist/images/demo/slider-1.jpg" alt="ew">
+	<h3>Opis 2</h3>
+	<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco. laboris nisi ut aliquip ex ea commodo consequat. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco. laboris nisi ut aliquip ex ea commodo consequat.</p>
+</div>
+
+<div class="partners-block">
+	<div class="text-center">
+		<p class="title-block_small-title">Nasza kompleksowa oferta</p>
+		<h2 class="title-block_big-title">Zaufali nam</h2>
+	</div>
+	
+	<div class="partners-wrapp pl-172 pr-172">
+		<div class="logos-line clearfix">
+			<a href="#" class="logo-wrapp">
+				<img src="assets/dist/images/partners/partner1.png" alt="d">
+			</a>
+			<a href="#" class="logo-wrapp">
+				<img src="assets/dist/images/partners/partner2.png" alt="d">
+			</a>
+			<a href="#" class="logo-wrapp">
+				<img src="assets/dist/images/partners/partner3.png" alt="d">
+			</a>
+			<a href="#" class="logo-wrapp">
+				<img src="assets/dist/images/partners/partner2.png" alt="d">
+			</a>
+			<a href="#" class="logo-wrapp">
+				<img src="assets/dist/images/partners/partner1.png" alt="d">
+			</a>
+			<a href="#" class="logo-wrapp">
+				<img src="assets/dist/images/partners/partner1.png" alt="d">
+			</a>
+			<a href="#" class="logo-wrapp">
+				<img src="assets/dist/images/partners/partner2.png" alt="d">
+			</a>
+			<a href="#" class="logo-wrapp">
+				<img src="assets/dist/images/partners/partner2.png" alt="d">
+			</a>
+			<a href="#" class="logo-wrapp">
+				<img src="assets/dist/images/partners/partner3.png" alt="d">
+			</a>
+			<a href="#" class="logo-wrapp">
+				<img src="assets/dist/images/partners/partner1.png" alt="d">
+			</a>
+			<a href="#" class="logo-wrapp">
+				<img src="assets/dist/images/partners/partner2.png" alt="d">
+			</a>
+			<a href="#" class="logo-wrapp">
+				<img src="assets/dist/images/partners/partner3.png" alt="d">
+			</a>
+			<a href="#" class="logo-wrapp">
+				<img src="assets/dist/images/partners/partner3.png" alt="d">
+			</a>
+		</div>
+
+	</div>
+
+</div>
+
+
+
+
+
+@endsection
+
+@section('page-scripts')
+<script>
+
+
+</script>
+@endsection
