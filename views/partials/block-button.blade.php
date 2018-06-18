21742b182fa60028b2d90a6eaaae0cda2bde9478
Bodya3927 <bodyahnedko@gmail.com>
2018-06-18 11:50:23 +0300

    зробив нижній блок ссилкою і баттоном

--- views/partials/block-button.blade.php
+++ views/partials/block-button.blade.php
@@ -1,9 +1,9 @@
-<div data-aos="zoom-in" class="aos inner-block_wrapp contact-btn clearfix pl-172 pr-172 hover-green">
+<button data-aos="zoom-in" class="aos inner-block_wrapp contact-btn clearfix pl-172 pr-172 hover-green">
 	<div class="inner-block">
 		<p class="title-block_small-title">Odpowiemy w ciągu 24h</p>
 		<h2 class="title-block_big-title fz48">Prześlij brief</h2>
 	</div>
 	<div class="inner-block text-right">
-		<button class="has-icon-btn text-uppercase">WYPEŁNIJ</button> 
+		<span class="has-icon-btn text-uppercase">WYPEŁNIJ</span> 
 	</div>
-</div>
+</button>
--- views/partials/block-link.blade.php
+++ views/partials/block-link.blade.php
@@ -1,10 +1,10 @@
 
-<div data-aos="zoom-in" class="aos inner-block_wrapp clearfix pl-172 pr-172 hover-green">
+<a href="#" data-aos="zoom-in" class="aos inner-block_wrapp clearfix pl-172 pr-172 hover-green">
 	<div class="inner-block">
 		<p class="title-block_small-title">Co możemy dla ciebie zrobić?</p>
 		<h2 class="title-block_big-title fz48">Wypełnij Brief</h2>
 	</div>
 	<div class="inner-block text-right">
-		<a href="#" class="has-icon-btn text-uppercase">WYPEŁNIJ</a>
+		<span href="#" class="has-icon-btn text-uppercase">WYPEŁNIJ</span>
 	</div>
-</div>
+</a>
