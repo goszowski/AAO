3fe1b19db36e73bfb823e891a6ef083dbeca273f
Jaroslaw Goszowski <goszowski@gmail.com>
2018-06-26 17:58:16 +0300

    merge

--- router.php
+++ router.php
@@ -1,25 +1,25 @@
 <?php 
 
-$router->get('/', function() use($blade) {
+$router->get('/krz', function() use($blade) {
 	return $blade->make('root.show');
 });
 
-$router->get('/brief', function() use($blade) {
+$router->get('/krz/brief', function() use($blade) {
 	return $blade->make('brief.show');
 });
 
-$router->get('/oferta', function() use($blade) {
+$router->get('/krz/oferta', function() use($blade) {
 	return $blade->make('oferta.show');
 });
 
-$router->get('/portfolio', function() use($blade) {
+$router->get('/krz/portfolio', function() use($blade) {
 	return $blade->make('portfolio.index');
 });
 
-$router->get('/portfolio/show', function() use($blade) {
+$router->get('/krz/portfolio/show', function() use($blade) {
 	return $blade->make('portfolio.show');
 });
 
-$router->get('/studio', function() use($blade) {
+$router->get('/krz/studio', function() use($blade) {
 	return $blade->make('studio.show');
 });
